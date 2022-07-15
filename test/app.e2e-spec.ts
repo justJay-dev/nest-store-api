ad4fec58c21557d88195bc2e5024940f2c1352e5
justJay-dev <jay@lowbrowlabs.com>
2022-07-15 10:13:19 -0400

    wip

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
+import { mockProducts } from '../src/products/tests/products.mocks';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -15,10 +15,34 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Core Module', () => {
+    it('/ (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello Nest Store API!!');
+    });
+
+    it('/health (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/health')
+        .expect(200)
+        .expect('OK');
+    });
+  });
+
+  describe('Carts Module', () => {
+    //todo this is fucked, do I want e2e in its own bucket?
+    //probably if im going to have many more modules, but, will i?
+    it('/carts (GET)', () => {
+      return request(app.getHttpServer()).get('carts').expect(200);
+    });
+    /* todo
+    it('/carts (POST)', ()=>{
+return request(app.getHttpServer())
+.post('/carts' )
+
+    })
+    */
   });
 });
